c0dff9950a9dd8e78fdc8784ebcda6576491bd2e
B032110058Hazim <B032110058@student.utem.edu.my>
2023-01-07 12:46:38 +0800

    Functional JavaScript now

--- script.js
+++ script.js
@@ -3,44 +3,86 @@ const ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 900;
 
-//mouse
+let frame = 0;
+
+// Mouse
 const mouse = {
     x: 10,
     y: 10,
     width: 0.1,
     height: 0.1,
 }
+
 let canvasPosition = canvas.getBoundingClientRect();
+
 canvas.addEventListener('mousemove', function(e) {
     mouse.x = e.x - canvasPosition.left;
     mouse.y = e.y - canvasPosition.top;
 });
+
 canvas.addEventListener('mouseleave', function(e) {
     mouse.x = undefined;
     mouse.y = undefined;
 });
 
-class Player {
+//
+
+let characters = [];
+
+class Character {
     constructor(x,y){
-    this.x = x;
-    this.y = y;
-    this.width = 0;
-    this.height = 0;
-    this.health = 100;
+        this.x = x;
+        this.y = y;
     }
+
     draw(){
-
+        console.log("draw");
     }
-    update() {
 
+    update() {
+        console.log("update");
     }
 };
 
+class Player extends Character{
+    constructor(x,y){
+        super(x,y)
+        this.width = 0;
+        this.height = 0;
+        this.health = 100;
+    }
+
+    draw(){
+        console.log("drawp");
+    }
+
+    update() {
+        console.log("updatep");
+    }
+};
+
+class Enemy extends Character{
+    constructor(x,y){
+        super(x,y)
+        this.width = 0;
+        this.height = 0;
+        this.health = 100;
+    }
+};
+
+characters.push(new Player(0, 0));
+characters.push(new Enemy(0, 0));
+
 function animate() {
     ctx.clearRect(0,0,canvas.width, canvas.height);
-    handleDefenders();
+
+    for (const c of characters){
+        c.update();
+        c.draw();
+    }
+    
     frame++;
-    if (!gameOver) requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 }
 animate();
 
